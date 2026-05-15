b2feb53d472c20f1097e15f6cd243d0db1361249
LittenPL <pedrolucasmoraisgouveia@gmail.com>
2026-05-15 16:10:57 -0400

    cmd move

--- ChiwabeChatbot/cmd.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cmd.exe /k cd /d "%~dp0"
-javac Celebro.java Memoria.java ChiwabeLLM.java
-java Celebro
---
+++ cmd.bat
@@ -0,0 +1,4 @@
+@echo off
+cmd.exe /k cd /d "%~dp0"
+javac Celebro.java Memoria.java ChiwabeLLM.java
+java Celebro
